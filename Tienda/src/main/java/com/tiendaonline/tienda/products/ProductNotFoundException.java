08374daf7bbbb875777e67a2117299d17e132882
MiguelGarzo <mgarzom@uoc.edu>
2026-01-19 15:37:25 +0100

    Just a Comment to Try

--- Tienda/src/main/java/com/tiendaonline/tienda/products/ProductNotFoundException.java
+++ Tienda/src/main/java/com/tiendaonline/tienda/products/ProductNotFoundException.java
@@ -3,6 +3,7 @@ package com.tiendaonline.tienda.products;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// Class to throw the 404 error.
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(Long id){
